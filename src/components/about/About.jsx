66f15b72364686568163b38cdb4e0fe95dd25ed5
Alan Senderovich <69826656+Alan-Senderovich@users.noreply.github.com>
2023-05-22 00:10:32 -0300

    add project

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./about.css";
 import ME from "../../assets/Alan-Senderovich.png";
 import { FaAward } from "react-icons/fa";
-import { FiUsers } from "react-icons/fi";
 import { VscFolderOpened } from "react-icons/vsc";
 
 const About = () => {
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -8,32 +8,41 @@ import IMGnemplaSite from "../../assets/nempla-site.PNG";
 import IMGfrontendMentor from "../../assets/frontend-mentor-ecommerce.PNG";
 import IMGinteractiveComments from "../../assets/interactive-comments.PNG";
 import IMGcountries from "../../assets/frontend-mentor-countries.png";
+import IMGurlShort from "../../assets/frontend-mentor-url-short.png";
 
 const data = [
   {
     id: 1,
     image: IMGdashboard,
-    title: "Admin Dashboard - React/MUI",
+    title: "Admin Dashboard",
+    subtitle: "",
+    description: "React & Material UI",
     github: "https://github.com/Alan-Senderovich/Dashboard-React-mui",
     demo: "https://dashboard-react-mui.vercel.app/",
   },
   {
     id: 2,
     image: IMGgoogleClone,
-    title: "Google clone fully responsive - NextJS & TailwindCSS",
+    title: "Google clone",
+    subtitle: "Fully Responsive",
+    description: "ReactJS & NextJS & TailwindCSS & Google API",
     github: "https://github.com/Alan-Senderovich/googleClone_nextTailwind",
     demo: "https://google-clone-next-tailwind-bige13b0b-alan-senderovich.vercel.app/",
   },
   {
     id: 3,
     image: IMGnemplaSite,
-    title: "WordPress site made with Elementor",
+    title: "Freelance project",
+    subtitle: "WordPress site",
+    description: "Made with Elementor",
     demo: "https://nempla.com/",
   },
   {
     id: 4,
     image: IMGfrontendMentor,
-    title: "FrontendMentor Challenge - E-commerce page - React & Tailwind",
+    title: "FrontendMentor Challenge",
+    subtitle: "E-commerce page",
+    description: "ReactJS & TailwindCSS",
     github:
       "https://github.com/Alan-Senderovich/Challenge-FrontendMentor-ecommerce-main-page",
     demo: "https://benevolent-bavarois-cad2ab.netlify.app/",
@@ -41,16 +50,31 @@ const data = [
   {
     id: 5,
     image: IMGinteractiveComments,
-    title: "FrontendMentor Challenge - Interactive Comments Page - React & Tailwind",
+    title: "FrontendMentor Challenge",
+    subtitle: "Interactive Comments Page",
+    description: "ReactJS & Tailwind",
     github: "https://github.com/Alan-Senderovich/Interactive-comments-page",
     demo: "https://gleeful-mandazi-45aacc.netlify.app/",
   },
   {
     id: 6,
     image: IMGcountries,
-    title: "FrontendMentor Challenge - Rest Countries - React & Next & TypeScript & Tailwind & API data fetching",
-    github: "https://github.com/Alan-Senderovich/FrontendMentor-countries-challenge",
+    title: "FrontendMentor Challenge",
+    subtitle: "Rest Countries",
+    description: "React & Next & TypeScript & Tailwind & API data fetching",
+    github:
+      "https://github.com/Alan-Senderovich/FrontendMentor-countries-challenge",
     demo: "https://frontend-mentor-countries-challenge-ashen.vercel.app/",
+  },
+  {
+    id: 7,
+    image: IMGurlShort,
+    title: "FrontendMentor Challenge",
+    subtitle: "Url shortening",
+    description: "React & Next & TypeScript & Tailwind & API data fetching",
+    github:
+      "https://github.com/Alan-Senderovich/FrontendMentor-url-shortening-api",
+    demo: "https://frontend-mentor-url-shortening-api-psi.vercel.app/",
   },
 ];
 
@@ -68,6 +92,8 @@ const Portfolio = () => {
                 <img src={item.image} alt="proj1" />
               </div>
               <h3>{item.title}</h3>
+              <h4>{item.subtitle}</h4>
+              <p>{item.description}</p>
               <div className="portfolio__item-cta">
                 <a
                   href={item.github}
